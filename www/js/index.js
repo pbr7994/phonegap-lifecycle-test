de2aab61e71853ede2a5682f778ea6066a230521
pbr7994 <brup1_13@uni.worc.ac.uk>
2014-10-17 15:27:59 +0100

    Update index.js
    flake info and JSONing?

--- www/js/index.js
+++ www/js/index.js
@@ -2,18 +2,38 @@ var paused_count = window.localStorage.getItem("paused");
 var resumed_count = window.localStorage.getItem("resumed");
 var launched_count = window.localStorage.getItem("launched");
 var item_count = window.localStorage.length;
+var flake = {
+	"Energy": {
+		"KJ": "2230kj",
+		"Kcal": "535kcal"
+		},
+	"Protein": "8.1g",
+	"Carbohydrate": "56g",
+	"Fat": {
+		"Saturated": "19g",
+		"UnSaturated": "11.5g"
+		},
+	"Fibre": "0.7g"
+	}
+var flake_string = JSON.stringify(flake);
+var local_flake = window.localStorage.setItem("flake_string")
+var getFlake = window.localStorage.getItem("flake_string")
+var JSON_flake = JSON.parse(getFlake)
+
 
     function onLoad() {
         document.addEventListener("deviceready", onDeviceReady, false);
 		console.log("device ready");
+		
     }
 	
 	function updateDisplay() {
 		$("#launched").text("Application launched: " + launched_count);
 		$("#resumed").text("Application paused: " + paused_count);
 		$("#paused").text("Application resumed: " + resumed_count);
-		item_count = window.localStorage.length
-		$("#count").text("There are " + item_count + " items being stored on the device for this app!")
+		item_count = window.localStorage.length;
+		$("#count").text("There are " + item_count + " items being stored on the device for this app!");
+		$("#flake").text(flake_string + <p> + "Energy " + JSON_flake.Energy.KJ + " / " + JSON_flake.Energy.Kcal);
 	}
 
 
